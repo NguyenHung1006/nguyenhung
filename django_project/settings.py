2bdc7ea6835d69c86d475e883d135e6b75d5c072
NguyenHung1006 <hung.nth206@gmail.com>
2020-07-19 22:09:44 +0700

    'd

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-#ALLOWED_HOSTS = ['webf.herokuapp.com', '127.0.0.1']
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nguyenhung2006.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
